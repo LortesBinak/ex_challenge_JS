cf150bccd7df76d4986619e94e65282a7bdfcd50
Sebastian <67074884+LortesBinak@users.noreply.github.com>
2020-11-18 21:30:08 +0100

    File Upload done
    Now it is possible to upload a csv file into the small application. Currently the file is output to the console.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
-const app = Vue.creatApp({
+const app = Vue.createApp({
     data:function(){
         return{
-            
+            selectedFile:null,
         }
     },
 
     methods:{
+        evaluateCsv(){
+            Papa.parse(this.selectedFile, {
+                complete: function(results){
+                    console.log("Finished:", results.data)
+                }
+            })
+        },
 
+        previewFiles(event){
+            this.selectedFile = event.target.files[0]
+        }
     }
 
 });
