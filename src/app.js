0f86b8a73a5dd74247b576ff079665068292f722
Sebastian <67074884+LortesBinak@users.noreply.github.com>
2020-11-19 10:07:40 +0100

    File reading done
    The Applications is now able to read the CSV file and output the important data

--- src/app.js
+++ src/app.js
@@ -2,21 +2,47 @@ const app = Vue.createApp({
     data:function(){
         return{
             selectedFile:null,
+            parsingStatus: false,
+            parsedFileArray: [],
+            parsedFileKeys: [],
+            parsedFileObject:{},
+            maximum: '',
         }
     },
 
     methods:{
-        evaluateCsv(){
-            Papa.parse(this.selectedFile, {
-                complete: function(results){
-                    console.log("Finished:", results.data)
-                }
-            })
-        },
-
         previewFiles(event){
             this.selectedFile = event.target.files[0]
+            console.log(this.selectedFile)
+        },
+
+        parseCsv(){
+            const that = this
+            Papa.parse(this.selectedFile, {
+                download:true,
+                header: true,
+                complete: function(results){
+                    that.getFileHeader(Object.keys(results.data[0]))
+                    results.data.map((data, index)=> {
+                      that.evaluateCsv(data)
+                    })
+                    
+                }
+            })
+            this.parsingStatus = !this.parsingStatus
+        },
+
+        getFileHeader(array){
+            this.parsedFileKeys = array
+            console.log(this.parsedFileKeys)
+        },
+
+        evaluateCsv(data){
+            this.parsedFileArray.push (data["MxT"])
+            console.log(this.parsedFileArray) 
         }
+
+       
     }
 
 });
